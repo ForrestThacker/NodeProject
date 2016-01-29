efd400bdfe9f83ef597409d393743e17cd017862
ForrestThacker <fyruskasai@gmail.com>
2016-01-29 12:23:58 -0700

    Added to the repo.

---
+++ src/Controller/NodeController.h
@@ -0,0 +1,15 @@
+/*
+ * NodeController.h
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: ftha4681
+ */
+
+#ifndef CONTROLLER_NODECONTROLLER_H_
+#define CONTROLLER_NODECONTROLLER_H_
+
+
+
+
+
+#endif /* CONTROLLER_NODECONTROLLER_H_ */
---
+++ src/Model/Node.h
@@ -0,0 +1,15 @@
+/*
+ * Node.h
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: ftha4681
+ */
+
+#ifndef MODEL_NODE_H_
+#define MODEL_NODE_H_
+
+
+
+
+
+#endif /* MODEL_NODE_H_ */
