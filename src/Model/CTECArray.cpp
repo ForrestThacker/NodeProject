884ee5001dc235de58cc67e3505336f7dbc0b869
ForrestThacker <fyruskasai@gmail.com>
2016-02-08 11:54:29 -0700

    Added to the repo.

--- src/Model/CTECArray.cpp
+++ src/Model/CTECArray.cpp
@@ -6,10 +6,18 @@
  */
 
 #include "CTECArray.h"
+using namespace std;
 
-CTECArray::CTECArray() {
+template<class Type>
+CTECArray<Type>::CTECArray(int size)
+{
+	this->size = size;
+	this->head = nullptr;
 	// TODO Auto-generated constructor stub
-
+	if(size <= 0)
+	{
+		cerr << "That is not allowed!" << endl;
+	}
 }
 
 CTECArray::~CTECArray() {
